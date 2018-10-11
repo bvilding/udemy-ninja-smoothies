7e006821f5cab838bef1367bc1db02ac2a2136cd
Vinni Hoke <vinni.hoke@gmail.com>
2018-10-11 12:06:24 -0700

    lesson-52

--- src/components/EditSmoothie.vue
+++ src/components/EditSmoothie.vue
@@ -1,6 +1,25 @@
 <template>
 	<div v-if="smoothie" class="edit-smoothie container">
 		<h2>Edit {{ smoothie.title }} Smoothie </h2>
+		<form @submit.prevent="editSmoothie">
+			<div class="field title">
+				<label for="title">Smoothie Title:</label>
+				<input type="text" name="title" v-model="smoothie.title" />
+			</div>
+			<div v-for="(ing, index) in smoothie.ingredients" :key="index" class="field">
+				<label for="ingredient">Ingredients:</label>
+				<input type="text" name="ingredient" v-model="smoothie.ingredients[index]" />
+				<i class="material-icons delete" @click="deleteIng(ing)">delete</i>
+			</div>
+			<div class="field add-ingredient">
+				<label for="add-ingredient">Add an Ingredient:</label>
+				<input type="text" name="add-ingredient" @keydown.tab.prevent="addIng" v-model="another" />
+			</div>
+			<div class="field center-align">
+				<p class="red-text" v-if="feedback"> {{ feedback }} </p>
+				<button class="btn pink" >Update Smoothie</button>
+			</div>
+		</form>
 	</div>
 </template>
 
@@ -11,7 +30,9 @@ export default {
   name: "EditSmoothie",
   data() {
     return {
-      smoothie: null
+      smoothie: null,
+      another: null,
+      feedback: null
     };
   },
   created() {
@@ -25,9 +46,52 @@ export default {
         this.smoothie.id = doc.id;
       });
     });
+  },
+  methods: {
+    addIng() {
+      if (this.another) {
+        //This is checking if the value is Null
+        this.smoothie.ingredients.push(this.another);
+        this.another = null;
+        this.feedback = null;
+      } else {
+        this.feedback = "You must enter a value to add an ingredient.";
+      }
+    },
+    deleteIng(ing) {
+      this.smoothie.ingredients = this.smoothie.ingredients.filter(
+        ingredient => {
+          return ingredient != ing;
+        }
+      );
+    },
+    editSmoothie() {
+      console.log(this.smoothie.title, this.smoothie.ingredients);
+    }
   }
 };
 </script>
 
 <style>
+.edit-smoothie {
+  margin-top: 60px;
+  padding: 20px;
+  max-width: 500px;
+}
+.edit-smoothie h2 {
+  font-size: 2em;
+  margin: 20px auto;
+}
+.edit-smoothie .field {
+  margin: 20px auto;
+  position: relative;
+}
+.edit-smoothie .delete {
+  position: absolute;
+  right: 0;
+  bottom: 16px;
+  color: #aaa;
+  font-size: 1.4em;
+  cursor: pointer;
+}
 </style>
